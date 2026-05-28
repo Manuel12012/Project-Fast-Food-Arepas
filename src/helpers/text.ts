b69515f80a0b7881b6fc97638d99866488a4e40a
Manuel Anthony Meza Rivas <manuelmezarivas120@gmail.com>
2026-05-27 23:09:43 -0500

    .

--- src/helpers/text.ts
+++ src/helpers/text.ts
@@ -1,12 +1,12 @@
 // src/helpers/text.js
 
-export const convertirAOracion = (texto) => {
+export const convertirAOracion = (texto: string) => {
   if (!texto) return "";
 
   return texto
     .toLowerCase()
     .split(" ")
-    .map((palabra) => {
+    .map((palabra : string) => {
       return palabra.charAt(0).toUpperCase() + palabra.slice(1);
     })
     .join(" ");
