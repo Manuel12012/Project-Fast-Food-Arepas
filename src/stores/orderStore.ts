acf3f9b9c6029831519a102adb76c9217c005a0d
Manuel Meza <manuelmezarivas120@gmail.com>
2026-08-12 12:30:56 -0500

    paginacion agregada

--- src/stores/orderStore.ts
+++ src/stores/orderStore.ts
@@ -1,36 +1,40 @@
-import { defineStore } from "pinia"
-import {api} from "@/services/api"
-import type { Order } from "@/types"
+import { defineStore } from "pinia";
+import { api } from "@/services/api";
+import type { Order } from "@/types";
 
 export const useOrderStore = defineStore("orders", {
   state: () => ({
     orders: [] as Order[],
-    loading: false
+    loading: false,
+    lastPage: 0,
+    currentPage: 1,
   }),
 
   actions: {
-    async fetchOrders() {
-      this.loading = true
+    async fetchOrders(page: number) {
+      this.loading = true;
 
       try {
-        const { data } = await api.get("/api/orders")
-        this.orders = data
+        const { data } = await api.get("/api/orders", {
+          params: {
+            page,
+          },
+        });
+        this.orders = data.data;
+        this.lastPage = data.last_page;
+        this.currentPage = data.current_page;
       } catch (error) {
-        console.error(error)
+        console.error(error);
       } finally {
-        this.loading = false
+        this.loading = false;
       }
     },
-    async updateOrderStatus(
-      orderId: number,
-      status: string
-    ) {
+    async updateOrderStatus(orderId: number, status: string, page: number) {
       await api.put(`/api/orders/${orderId}`, {
-        status
-      })
+        status,
+      });
 
-      await this.fetchOrders()
-    }
-
-  }
-})
+      await this.fetchOrders(page);
+    },
+  },
+});
--- src/views/admin/AdminOrders.vue
+++ src/views/admin/AdminOrders.vue
@@ -1,25 +1,46 @@
 <script setup lang="ts">
-import { onMounted, reactive } from "vue";
+import { computed, onMounted, reactive } from "vue";
 import { useOrderStore } from "@/stores/orderStore";
 import NavBarAdmin from "@/components/admin/NavBarAdmin.vue";
 import AsideAdmin from "@/components/admin/AsideAdmin.vue";
 import { formatDate } from "@/helpers/formatDate";
+import { ref } from "vue";
 
 const orderStore = useOrderStore();
-
+const currentPage = ref(1);
 onMounted(async () => {
-  await orderStore.fetchOrders();
+  await orderStore.fetchOrders(currentPage);
   orderStore.orders.forEach((order) => {
     statusDraft[order.id] = order.status;
   });
 });
 
+const pages = computed(() => {
+  return Array.from({ length: orderStore.lastPage }, (_, index) => index + 1);
+});
+const nextPage = async () => {
+  if (orderStore.currentPage < orderStore.lastPage) {
+    orderStore.currentPage++;
+    await orderStore.fetchOrders(orderStore.currentPage);
+  }
+};
+
+const goToPage = async (page: number) => {
+  await orderStore.fetchOrders(page);
+};
+
+const prevPage = async () => {
+  if (orderStore.currentPage > 1) {
+    orderStore.currentPage--;
+    await orderStore.fetchOrders(orderStore.currentPage);
+  }
+};
 const statusDraft = reactive<Record<number, string>>({});
 
-const updateStatus = async (orderId: number, status: string | undefined) => {
+const updateStatus = async (orderId: number, status: string | undefined, page: number) => {
   if (!status) return;
 
-  await orderStore.updateOrderStatus(orderId, status);
+  await orderStore.updateOrderStatus(orderId, status, page);
   statusDraft[orderId] = status;
 };
 
@@ -361,6 +382,33 @@ const statusIcon = (status: string | undefined) => {
               </table>
             </div>
           </div>
+        </div>
+        <div class="flex gap-5">
+          <button
+            @click="prevPage"
+            :disabled="orderStore.currentPage === 1"
+            class="border border-gray-500 px-2 rounded"
+          >
+            Anterior
+          </button>
+          <div class="flex gap-2">
+            <button
+              v-for="page in pages"
+              v-bind:key="page"
+              @click="goToPage(page)"
+              class="border border-gray-500 px-2 rounded"
+            >
+              {{ page }}
+            </button>
+          </div>
+
+          <button
+            @click="nextPage"
+            :disabled="orderStore.currentPage === orderStore.lastPage"
+            class="border border-gray-500 px-2 rounded"
+          >
+            Siguiente
+          </button>
         </div>
       </div>
     </main>
